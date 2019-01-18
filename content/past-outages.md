bb1b5940063e5d5c9d907425feaa6622d5db219d
Jack Laxson <jackjrabbit@gmail.com>
2019-01-18 13:30:54 -0500

    migrate old outage info

---
+++ content/past-outages.md
@@ -0,0 +1,7 @@
+Title: Past Outages
+Date: 2019-1-18
+Category: Outages
+
+ * 1/17/2019 10:30 pm EST moved loomio to demos. db is a live copy, contact the web team if you're missing data we have it in a backup.
+ * We had degraded service on loomio reported on 1/16/2019. Fixed around 1pm EST. We goofed with a db operation that didn't do a cascading delete.
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+         ('Earth Strike International', 'htts://earth-strike.com/'),
+         ('Loomio', 'https://loomio.earth-strike.com/'),
+         ('Zulip', 'https://zulip.earth-strike.com/'),
+        )
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
